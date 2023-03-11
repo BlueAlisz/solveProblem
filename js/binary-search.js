66ccf4b07e25a3047d9864a9e01aae2cc4232da0
Nattaphat Yadfung <nattaphatblue.2545@gmail.com>
2023-03-12 03:33:50 +0700

    update: binarySearch

---
+++ js/binary-search.js
@@ -0,0 +1,29 @@
+function binlarySearch(nums, target) {
+    var start = 0;
+    var end = nums.length - 1;
+    while (start <= end) {
+        var mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+    }
+    console.log("hello");
+    return -1;
+}
+//binarySearch([15,58,96], 58)
---
+++ ts/binary-search.ts
@@ -0,0 +1,29 @@
+function binlarySearch(nums: number[], target: number): number {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    console.log("hello")
+    return -1;
+}
+
+//binarySearch([15,58,96], 58)
